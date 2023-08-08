7f9bbd5a9fdd678737cd7b9d58c38ddf73762563
Zoe Cohen <cohen.zoe@berkeley.edu>
2023-08-08 16:51:46 +0000

    check boxes, update movies displayed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    # if ratings_list is nil, retrieve ALL movies
+    if ratings_list.nil?
+      all
+    else
+      Movie.where(rating: ratings_list.map(&:upcase))
+    end
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+  end
 end
